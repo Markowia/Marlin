57dc7754387e9b5394878c50fa04131759c95f05
Markowia <markowi4@gmail.com>
2024-01-08 22:05:19 +0200

    Ender-3 Base V8

---
+++ Marlin/_Statusscreen.h
@@ -0,0 +1,78 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
+ * Custom Status Screen bitmap
+ *
+ * Place this file in the root with your configuration files
+ * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
+ *
+ * Use the Marlin Bitmap Converter to make your own:
+ * https://marlinfw.org/tools/u8glib/converter.html
+ */
+
+//
+// Status Screen Logo bitmap
+//
+#define STATUS_LOGO_Y            8
+#define STATUS_LOGO_WIDTH       40
+
+const unsigned char status_logo_bmp[] PROGMEM = {
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B00000000,B00011110,B00111110,B00000000,B00000000,
+  B00000000,B00100011,B00110011,B00000000,B00000000,
+  B00000000,B00000011,B00110011,B00000000,B00000000,
+  B00000000,B00011110,B00110011,B00000000,B00000000,
+  B00000000,B00000011,B00110011,B00000000,B00000000,
+  B00000000,B00000011,B00110011,B00000000,B00000000,
+  B00000000,B00100011,B00110011,B00000000,B00000000,
+  B00000000,B00011110,B00111110,B00000000,B00000000,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B10001000,B00000000,B00000000,B01000000,B00000000,
+  B10001000,B00000000,B00000000,B00000000,B00000000,
+  B11011001,B11001111,B00011101,B11000011,B10011110,
+  B11011000,B00101100,B10100000,B01000100,B01011001,
+  B10101001,B11101000,B10110000,B01000100,B01010001,
+  B10001010,B00101000,B10001100,B01000100,B01010001,
+  B10001010,B01101000,B10000100,B01000100,B01010001,
+  B10001001,B11101000,B10111001,B11110011,B10010001,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+  B11111111,B11111111,B11111111,B11111111,B11111111,
+  B00110011,B00110011,B00110011,B00110011,B00110011,
+  B11111111,B11111111,B11111111,B11111111,B11111111,
+  B00000000,B00000000,B00000000,B00000000,B00000000,
+};
+
+//
+// Use default bitmaps
+//
+#define STATUS_HOTEND_ANIM
+#define STATUS_BED_ANIM
+#define STATUS_HEATERS_XSPACE   20
+#if HOTENDS < 2
+  #define STATUS_HEATERS_X      48
+  #define STATUS_BED_X          72
+#else
+  #define STATUS_HEATERS_X      40
+  #define STATUS_BED_X          80
+#endif
